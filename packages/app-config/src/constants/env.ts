4631b5ae27f693abb664ae71e3ec0ab001ea50d9
Zhichao Zhang <zzcwoshizz@163.com>
2022-07-19 16:37:50 +0800

    gh ci use version

--- packages/app-config/src/constants/env.ts
+++ packages/app-config/src/constants/env.ts
@@ -7,5 +7,5 @@ export const env: {
     (process.env.CREDENTIAL_SERVICE as string) || 'https://credential-service.starks.network',
   ZKID_SERVICE: (process.env.ZKID_SERVICE as string) || 'https://zkid-service.starks.network',
   MESSAGE_WS_URL:
-    (process.env.MESSAGE_WS_URL as string) || 'wss://wss.credential-service.starks.network/ws'
+    (process.env.MESSAGE_WS_URL as string) || 'wss://wss.credential-service.starks.network'
 };
