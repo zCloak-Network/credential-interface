b7801fa379e1f76b4625d72ff585793d0b74b641
zzcwoshizz <zzcwoshizz@163.com>
2022-07-20 13:46:25 +0800

    Unlock modal UI (#25)
    * import icon-park
    
    * 1. dialog style
    2. unlock modal
    
    * ci update

--- packages/react-dids/src/UnlockModal.tsx
+++ packages/react-dids/src/UnlockModal.tsx
@@ -1,19 +1,10 @@
 import { DidUri } from '@kiltprotocol/sdk-js';
-import { LoadingButton } from '@mui/lab';
-import {
-  Dialog,
-  DialogActions,
-  DialogContent,
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  Typography
-} from '@mui/material';
+import LockIcon from '@mui/icons-material/Lock';
+import PersonIcon from '@mui/icons-material/Person';
+import { Button, Dialog, DialogContent, InputAdornment, OutlinedInput, Stack } from '@mui/material';
 import React, { useCallback, useState } from 'react';
 
 import { DialogHeader, InputPassword } from '@credential/react-components';
-
-import DidName from './DidName';
 
 const UnlockModal: React.FC<{
   open: boolean;
@@ -23,7 +14,6 @@ const UnlockModal: React.FC<{
   onUnlock: () => void;
 }> = ({ did, onClose, onUnlock, open, unlockDid }) => {
   const [password, setPassword] = useState<string>();
-  const [error, setError] = useState<Error>();
 
   const _onUnlock = useCallback(async () => {
     if (!did) return;
@@ -33,32 +23,39 @@ const UnlockModal: React.FC<{
       await unlockDid(did, password);
 
       onUnlock();
-    } catch (error) {
-      setError(error as Error);
-    }
+    } catch (error) {}
   }, [did, onUnlock, password, unlockDid]);
 
   return (
     <Dialog maxWidth="md" onClose={onClose} open={open}>
       <DialogHeader onClose={onClose}>Unlock account</DialogHeader>
-      <DialogContent>
-        <Typography mb={2} variant="h4">
-          Please input password unlock
-        </Typography>
-        <Typography mb={4} variant="inherit">
-          <DidName shorten={false} value={did} />
-        </Typography>
-        <FormControl error={!!error} fullWidth variant="outlined">
-          <InputLabel shrink>Please input password</InputLabel>
-          <InputPassword onChange={(e) => setPassword(e.target.value)} />
-          {error && <FormHelperText>Unable to unlock account</FormHelperText>}
-        </FormControl>
+      <DialogContent sx={{ width: '578px', maxWidth: '100%' }}>
+        <Stack spacing={4}>
+          <OutlinedInput
+            disabled
+            fullWidth
+            startAdornment={
+              <InputAdornment position="start">
+                <PersonIcon color="primary" />
+              </InputAdornment>
+            }
+            value={did}
+          />
+          <InputPassword
+            fullWidth
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Please input password"
+            startAdornment={
+              <InputAdornment position="start">
+                <LockIcon color="primary" />
+              </InputAdornment>
+            }
+          />
+          <Button fullWidth onClick={_onUnlock} variant="contained">
+            Unlock
+          </Button>
+        </Stack>
       </DialogContent>
-      <DialogActions>
-        <LoadingButton onClick={_onUnlock} variant="contained">
-          Unlock
-        </LoadingButton>
-      </DialogActions>
     </Dialog>
   );
 };
