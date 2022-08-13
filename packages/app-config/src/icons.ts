0a6430ea56b672b675d15829695e162191f1012c
zzcwoshizz <zzcwoshizz@163.com>
2022-08-13 12:27:14 +0800

    retweet (#38)

--- packages/app-config/src/icons.ts
+++ packages/app-config/src/icons.ts
@@ -3,6 +3,7 @@ export { default as LogoBlackIcon } from './assets/logo_black.svg';
 export { default as LogoCircleIcon } from './assets/logo_circle.svg';
 export { default as IconDownload } from './assets/icon_download.svg';
 export { default as IconForward } from './assets/icon_forward.svg';
+export { default as IconTwitter } from './assets/icon_twitter.svg';
 export { default as IconImport } from './assets/icon_import.svg';
 export { default as IconNewMessage } from './assets/icon_new_message.svg';
 export { default as IconNewTask } from './assets/icon_new_task.svg';
